5078f9bbbf967603ff131e8adc89f4c659c362aa
Jan Alexander Slabiak <j.slabiak@telekom.de>
2013-05-31 13:09:46 +0200

    Basic decryption and mount with cryptsetup
    Created basic implementation of a 'better' way of mount a block devices
    via cryptsetup.
    
    It's write to run only with the mount-dmcrypt.pp, files to turn it later into a module are created.
    The manifest will check for all variables.
     <mount_point> does it exist?
     <device> is it a real luks devices?
     <dmcrypt_password> ToDO getting it from the Puppetmaster via puppet-secret
     <dmcrypt_name> Do we have it?
    
    There are some more ToDo open.
    
     - Creating a documentation.
     - Checking that the decryption process isn't logged to the local system or:
     - Ensure this is secure enough.
     - Turn it into a real puppet module.
     - Enable puppet-lint for more beauty.
     - Release it on github.
     - Stuff that I miss?

---
+++ dmcrypt/README.md
@@ -0,0 +1,8 @@
+README.md
+
+
+
+
+## Version ##
+
+Test on a Ubuntu 12.04 with Puppet 3.2.1 and cryptsetup 1.4.1.
--- dmcrypt/manifests/mount-dmcrypt.pp
+++ dmcrypt/manifests/mount-dmcrypt.pp
@@ -4,7 +4,7 @@
   # This will test a block devices to be a vailded dmcrypt devices, 
   # then try to mount it.
   # Need at least two variables.
-  # <mount_point><dmcrypt_password>[<dmcrypt_name>]
+  # <mount_point><dmcrypt_device> <dmcrypt_password>[<dmcrypt_name>]
   #
   # Additonaly we can add a name for the dmcrypt. Else it will called dmcrypt-devcies.
   # /dev/mapper/dmcrypt-sda
@@ -23,7 +23,7 @@
 
  define block_devices_test($device) {
      exec { "is_true_luksdevice":
-        #require => Package['cryptsetup'],
+        require => Package['cryptsetup'],
         command => "/sbin/cryptsetup isLuks /dev/$device",
      }
     }
@@ -60,7 +60,7 @@
 #:
 # arguments
 #
-define decrypt_and_mount ($input_device, $input_mount_point, $input_dmcrypt_name) {
+define decrypt_and_mount ($input_device, $input_mount_point, $input_dmcrypt_name, $input_dmcrypt_key) {
 
     block_devices_test {'test_sdb': device => "$input_device"}
     mount_point_test { 'test_osd_mountpoint_osd-1': mount_point => "${input_mount_point}"}
@@ -69,18 +69,22 @@ define decrypt_and_mount ($input_device, $input_mount_point, $input_dmcrypt_name
     exec { "decrypt_dmcrypt_device":
         require => Exec['is_existing_mount_point', 'is_true_luksdevice'],
         unless => "/sbin/cryptsetup status /dev/mapper/${input_dmcrypt_name}",
-        command => "/sbin/cryptsetup luksOpen /dev/${device} ${input_dmcrypt_name}",
+        command => "/bin/echo -n ${input_dmcrypt_key} |/sbin/cryptsetup luksOpen /dev/${input_device} ${input_dmcrypt_name} -d -",
     }
     exec { "mount_dmcrypt_device":
         require => Exec['decrypt_dmcrypt_device'],
-        unless => "/bin/mount |grep '/dev/mapper/${input_dmcrypt_name}",
-        command => "/bin/mount /dev/mapper/${input_dmcrypt_name} ${mount_point}",
+        unless => "/bin/mount |grep '/dev/mapper/${input_dmcrypt_name}'",
+        command => "/bin/mount /dev/mapper/${input_dmcrypt_name} ${input_mount_point}",
     }
 }
 
 
 # execute it for a test.
-
+#dmcrypt::core{}
+  package {'cryptsetup':
+    ensure => installed
+  }
 decrypt_and_mount { 'mount_ceph_osd-1': input_device => 'sdb',
                                         input_mount_point => '/var/lib/ceph/osd/ceph-1',
-                                        input_dmcrypt_name => 'CEPH-OSD.1' }
+                                        input_dmcrypt_name => 'CEPH-OSD.1',
+                                        input_dmcrypt_key => 'asdfghjkl', }
