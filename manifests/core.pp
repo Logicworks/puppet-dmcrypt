4849bb2be1ec1269317446d9fa3033d32404ef8f
Jan Alexander Slabiak <j.slabiak@telekom.de>
2013-06-14 16:10:44 +0200

    Spliting Class for better usage

---
+++ manifests/core.pp
@@ -0,0 +1,19 @@
+##
+# This class provides support for mounting LUKS encrypted devices.
+#
+# Usage:
+#
+#  class {'dmcrypt': }
+#
+#  dmcrypt::luksDevice {'/dev/vdb1':
+#    name        => 'osd-1',
+#    mount_point => '/var/lib/ceph/osd/ceph-1'
+#  }
+#
+# ... and probably more calls to dmcrypt::luksDevice
+#
+class dmcrypt::core {
+  package {['cryptsetup', 'xfsprogs','wipe']:
+    ensure => installed
+  }
+}
