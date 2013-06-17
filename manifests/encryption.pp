2eda39a8354c131688b3419f0bf06e15386d396b
Jan Alexander Slabiak <j.slabiak@telekom.de>
2013-06-17 09:53:41 +0200

    Create file for creation of a encryped hdd

---
+++ manifests/encryption.pp
@@ -0,0 +1,37 @@
+# Class: dmcrypt::encryption
+#
+#
+class dmcrypt::encryption {
+    # resources
+
+  $device = $title
+
+  $secret_path = "puppet:///secrets/$name"
+  $key_file = "/root/${name}.key"
+
+  file {$key_file:
+    ensure  => present,
+    mode    => '0600',
+    source  => $secret_path,
+  }
+
+define dmcrypt::luksFormat($key_file) {
+  $device = $title
+
+
+  exec {"luksFormat-${device}":
+    path    => '/bin:/usr/bin:/usr/sbin:/sbin',
+    command => "cryptsetup -q \
+                --cipher aes-xts-plain64 \
+                --key-size 512 \
+                --hash sha512 \
+                --use-urandom \
+                --key-file ${key_file} \
+                luksFormat ${device}",
+    onlyif  => "test f1c9645dbc14efddc7d8a322685f26eb = \
+                $(dd if=${device} bs=1k count=10k 2>/dev/null \
+                | md5sum - | cut -f1 -d' ')",
+    require => Package['cryptsetup']
+  }
+}
+}
