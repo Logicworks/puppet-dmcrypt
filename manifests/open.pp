cd63eb55c68f9d6deb0dc6af1c0caf0507a3b981
Jan Alexander Slabiak <j.slabiak@telekom.de>
2013-06-17 12:54:48 +0200

    Class to open luks devices

---
+++ manifests/open.pp
@@ -0,0 +1,9 @@
+
+class dmcrypt::encryption($device, $name){
+
+  $key_file = "/root/${device}.key"
+  exec {"luksOpen-${device}":
+    command => "/sbin/cryptsetup luksOpen /dev/${device} ${name} -d ${key_file}",
+    creates => "/dev/mapper/${name}",
+    require => Package['cryptsetup']
+}
